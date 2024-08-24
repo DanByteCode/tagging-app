a5a718d071aa8162798bb7563d4aa431b69bd59d
Darknhyt <40185738+Darknhyt@users.noreply.github.com>
2024-08-24 16:11:43 -0400

    First version

---
+++ src/app/about/page.tsx
@@ -0,0 +1,29 @@
+export default function About() {
+  return (
+    <main className="about">
+      <h2>About</h2>
+      <p>
+        Danbyte: This is my first Project in Next.js applying all the different
+        features of this framework.
+      </p>
+      <div>
+        <h3>Source</h3>
+        <p>
+          GitHub Repository:{' '}
+          <a href="https://github.com/DanByteCode/tagging-app" target="_blank">
+            Tagging-App
+          </a>
+        </p>
+      </div>
+      <div>
+        <h3>Images</h3>
+        <p>
+          Behance Gallery:{' '}
+          <a href="https://www.behance.net/gallery/298730/isometric-works">
+            Isometric Works
+          </a>
+        </p>
+      </div>
+    </main>
+  )
+}
